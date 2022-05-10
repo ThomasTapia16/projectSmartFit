1a191209f71f3fc1384db9ee6a2aaae115896d8a
ThomasTapia16 <thomastlsoftware@gmail.com>
2022-05-10 13:50:00 -0400

    first commit

---
+++ ProyectoSmartFit/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class TestController {
+	
+	
+	@RequestMapping("/")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+}
