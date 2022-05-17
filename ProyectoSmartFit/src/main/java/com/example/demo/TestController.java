79ee86a6d87e16309e25762caae6e1b6f3faa498
ThomasTapia16 <thomastlsoftware@gmail.com>
2022-05-17 18:13:14 -0400

    model

--- ProyectoSmartFit/src/main/java/com/example/demo/TestController.java
+++ ProyectoSmartFit/src/main/java/com/example/demo/TestController.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Service.DtiService;
+import com.example.demo.models.Colaborador;
+import com.example.demo.models.DTI;
+
+
+
 
 @Controller
 public class TestController {
 	
-	
+		
 	@RequestMapping("/")
 	public String login() {
 		return "login";
@@ -21,7 +33,8 @@ public class TestController {
 	
 	@GetMapping("/crearColaborador")
 	public String crearColaborador()
-	{
+	{	
+		
 		return "crearColaborador";
 	}
 	@GetMapping("/crearAdministrador")
